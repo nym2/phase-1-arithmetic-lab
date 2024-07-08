4556ab4209751fe9d5ccaa9b763d40d774269fa6
Nympha Pamba <pambapraise@gmail.com>
2024-07-08 17:27:32 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random()*10)+1;
+
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+
+const max = Math.max(4, 10, 15, 17, 20)
+console.log(max)
